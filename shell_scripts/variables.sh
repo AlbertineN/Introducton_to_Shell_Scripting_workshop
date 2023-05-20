2d65fa5adbefc529dd09d26892a34c51e4112be9
AlbertineN <albertinengingan@gmail.com>
2023-05-20 11:16:33 +0300

    Tried out user defined variables shell script

--- shell_scripts/variables.sh
+++ shell_scripts/variables.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/bash
 
+#system defined variables
 echo "User: $USER"
 echo "Working Directory: $PWD"
 
+
+# User defined variables
+AGE=18
+NAME="Albertine"
+
+
+echo "My name is $NAME . I am $AGE years old."
